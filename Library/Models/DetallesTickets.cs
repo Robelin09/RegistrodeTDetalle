f1dc03ea95ac1327b992c78bfc61f577ac06f3c9
Robelin09 <concepcionrobelin@gmail.com>
2024-02-25 18:51:19 -0400

    Creé la clase DetallesTickets en el Models

---
+++ Library/Models/DetallesTickets.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class DetallesTickets
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "El campo Emisor es obligatorio.")]
+        public string? Emisor { get; set; }
+
+        [Required(ErrorMessage = "El campo Mensaje es obligatorio.")]
+        public string? Mensaje { get; set; }
+    }
+}
