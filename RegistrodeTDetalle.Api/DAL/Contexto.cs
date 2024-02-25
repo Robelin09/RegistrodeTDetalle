f23808ab2a27cdf562e4cbbeecfa8a24db585088
Robelin09 <concepcionrobelin@gmail.com>
2024-02-25 18:55:50 -0400

    Creé la carpeta DAL con su clase Contexto

---
+++ RegistrodeTDetalle.Api/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Library.Models;
+
+namespace RegistrodeTDetalle.Api.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<DetallesTickets> DetallesTickets { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
