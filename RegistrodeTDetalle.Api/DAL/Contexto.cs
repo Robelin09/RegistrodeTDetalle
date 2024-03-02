f45d1ce3eec2c66f28e482788a19fd21ee0f804b
Robelin09 <concepcionrobelin@gmail.com>
2024-03-01 22:54:54 -0400

    Eliminé el objeto DetallesTickets

--- RegistrodeTDetalle.Api/DAL/Contexto.cs
+++ RegistrodeTDetalle.Api/DAL/Contexto.cs
@@ -6,7 +6,6 @@ namespace RegistrodeTDetalle.Api.DAL
     public class Contexto : DbContext
     {
         public DbSet<Tickets> Tickets { get; set; }
-        public DbSet<DetallesTickets> DetallesTickets { get; set; }
 
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     }
